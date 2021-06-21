81ad010adf685f751601f3662770abbf2b4633d3
Azhot <francois.jouvelot@gmail.com>
2021-06-21 15:48:50 +0200

    Create project

---
+++ app/src/main/java/fr/azhot/notes/MainActivity.kt
@@ -0,0 +1,21 @@
+package fr.azhot.notes
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import fr.azhot.notes.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+    // variables
+    private lateinit var binding: ActivityMainBinding
+
+
+    // overridden functions
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+    }
+}
